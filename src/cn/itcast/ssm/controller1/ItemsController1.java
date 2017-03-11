d145eb83f040874e7fbb61bf5834155a7b8d1293
houxuefeng <m18821753787_1@163.com>
2017-03-11 19:44:52 +0800

    拦截器的定义和配置全局拦截器

--- src/cn/itcast/ssm/controller1/ItemsController1.java
+++ src/cn/itcast/ssm/controller1/ItemsController1.java
@@ -1,14 +1,10 @@
 package cn.itcast.ssm.controller1;
-
 import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
 import javax.servlet.http.HttpServletRequest;
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,12 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
 import cn.itcast.ssm.po.ItemsCustom;
 import cn.itcast.ssm.po.ItemsQueryVo;
 import cn.itcast.ssm.service.ItemsService;
 import cn.itcast.ssm.validator.ValidGroup;
-
 /**
  * 
  */
---
+++ src/cn/itcast/ssm/interceptor/HandlerInterceptor1.java
@@ -0,0 +1,45 @@
+package cn.itcast.ssm.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+* @author  CreateBy HouXueFeng
+* @version 2017年3月11日 下午7:03:51
+* @description 拦截器类中的方法执行顺序
+* 
+*/
+public class HandlerInterceptor1 implements HandlerInterceptor{
+
+	
+	//进入handler之前执行
+	//用于身份认证，身份授权
+	//比如身份认证，如果认证不通过表示当前用户没有登录，需要此方法拦截不在向下执行。
+	
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		return false;//表示拦截，不向下执行
+//		return true;//表示执行
+	}
+
+	//进入handler之后，返回modelAndView之前执行
+	//应用场景从modeAndView出发（将公用的模型数据在这里传到视图，也可以在这里统一制定视图）
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+	}
+	//执行handler完成此方法
+	//应用场景：统一异常处理，统一日志处理。
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+	}
+
+}
